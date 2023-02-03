9f9e168f575298ea538d67f2b61ce8ee4d2df783
yosaddis <60548252+yosaddis@users.noreply.github.com>
2023-02-03 12:30:20 +0300

    Fix js and css files for linter errors

--- java_script.js
+++ java_script.js
@@ -4,101 +4,100 @@ const mobileMenuCloseBtn = document.querySelector('.mobile-menu-close-btn');
 const linkMyWorks = document.querySelector('.link-my-works');
 const linkAboutMe = document.querySelector('.link-about-me');
 const linkContactMe = document.querySelector('.link-contact-me');
-const popupBtns= document.querySelectorAll('.pop-up');
+const popupBtns = document.querySelectorAll('.pop-up');
 
 const myWorksContent = [
-	{
-		id: 0,
-		url: './img/background/bk.png',
-		alt: 'Project preview image',
-		title: 'My Works Title 1',
-		description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea`,
-		tecnologies: {
-			tech1: 'CodeKit',
-			tech2: 'GitHub',
-			tech3: 'Javascript',
-			tech4: 'Bootstrap',
-		},
-		linkVersion: '#',
-		linkSource: '#',
-	},
-	{
-		id: 1,
-		url: './img/background/bk.png',
-		alt: 'Project preview image',
-		title: 'My Works Title 2',
-		description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea`,
-		tecnologies: {
-			tech1: 'CodeKit',
-			tech2: 'GitHub',
-			tech3: 'Javascript',
-			tech4: 'Bootstrap',
-		},
-		linkVersion: '#',
-		linkSource: '#',
-	},
-	{
-		id: 2,
-		url: './img/background/bk.png',
-		alt: 'Project preview image',
-		title: 'My Works Title 3',
-		description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea`,
-		tecnologies: {
-			tech1: 'CodeKit',
-			tech2: 'GitHub',
-			tech3: 'Javascript',
-			tech4: 'Bootstrap',
-		},
-		linkVersion: '#',
-		linkSource: '#',
-	},
-	{
-		id: 3,
-		url: './img/background/bk.png',
-		alt: 'Project preview image',
-		title: 'My Works Title 4',
-		description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea`,
-		tecnologies: {
-			tech1: 'CodeKit',
-			tech2: 'GitHub',
-			tech3: 'Javascript',
-			tech4: 'Bootstrap',
-		},
-		linkVersion: '#',
-		linkSource: '#',
-	},
-	{
-		id: 4,
-		url: './img/background/bk.png',
-		alt: 'Project preview image',
-		title: 'My Works Title 5',
-		description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea`,
-		tecnologies: {
-			tech1: 'CodeKit',
-			tech2: 'GitHub',
-			tech3: 'Javascript',
-			tech4: 'Bootstrap',
-		},
-		linkVersion: '#',
-		linkSource: '#',
-	},
-	{
-		id: 5,
-		url: './img/background/bk.png',
-		alt: 'Project preview image',
-		title: 'My Works Title 6',
-		description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea`,
-		tecnologies: {
-			tech1: 'CodeKit',
-			tech2: 'GitHub',
-			tech3: 'Javascript',
-			tech4: 'Bootstrap',
-		},
-		linkVersion: '#',
-		linkSource: '#',
-	},
+  {
+    id: 0,
+    url: './img/background/bk.png',
+    alt: 'Project preview image',
+    title: 'My Works Title 1',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    tecnologies: {
+      tech1: 'CodeKit',
+      tech2: 'GitHub',
+      tech3: 'Javascript',
+      tech4: 'Bootstrap',
+    },
+    linkVersion: '#',
+    linkSource: '#',
+  },
+  {
+    id: 1,
+    url: './img/background/bk.png',
+    alt: 'Project preview image',
+    title: 'My Works Title 2',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    tecnologies: {
+      tech1: 'CodeKit',
+      tech2: 'GitHub',
+      tech3: 'Javascript',
+      tech4: 'Bootstrap',
+    },
+    linkVersion: '#',
+    linkSource: '#',
+  },
+  {
+    id: 2,
+    url: './img/background/bk.png',
+    alt: 'Project preview image',
+    title: 'My Works Title 3',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    tecnologies: {
+      tech1: 'CodeKit',
+      tech2: 'GitHub',
+      tech3: 'Javascript',
+      tech4: 'Bootstrap',
+    },
+    linkVersion: '#',
+    linkSource: '#',
+  },
+  {
+    id: 3,
+    url: './img/background/bk.png',
+    alt: 'Project preview image',
+    title: 'My Works Title 4',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    tecnologies: {
+      tech1: 'CodeKit',
+      tech2: 'GitHub',
+      tech3: 'Javascript',
+      tech4: 'Bootstrap',
+    },
+    linkVersion: '#',
+    linkSource: '#',
+  },
+  {
+    id: 4,
+    url: './img/background/bk.png',
+    alt: 'Project preview image',
+    title: 'My Works Title 5',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    tecnologies: {
+      tech1: 'CodeKit',
+      tech2: 'GitHub',
+      tech3: 'Javascript',
+      tech4: 'Bootstrap',
+    },
+    linkVersion: '#',
+    linkSource: '#',
+  },
+  {
+    id: 5,
+    url: './img/background/bk.png',
+    alt: 'Project preview image',
+    title: 'My Works Title 6',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    tecnologies: {
+      tech1: 'CodeKit',
+      tech2: 'GitHub',
+      tech3: 'Javascript',
+      tech4: 'Bootstrap',
+    },
+    linkVersion: '#',
+    linkSource: '#',
+  },
 ];
-
 
 const popupDiv = (index) => `
  <section class="my-works-details">
@@ -106,19 +105,19 @@ const popupDiv = (index) => `
               <img class="icon" src="./img/icon/Icon-cancel-menu.svg" alt="cancel icon">
           </button>
           <div class="work-image-details">
-        		<img src=${myWorksContent[index].url} alt ='${myWorksContent[index].alt}'>
+          <img src=${myWorksContent[index].url} alt ='${myWorksContent[index].alt}'>
           </div>
           <h2 class="work-title-mobile">
               ${myWorksContent[index].title}
           </h2>
-          <ul class="work-tags-mobile">
+          <ul class="work-tags-mobile">s
               <li class="tag-mobile">${myWorksContent[index].tecnologies.tech1}</li>
               <li class="tag-mobile">${myWorksContent[index].tecnologies.tech2}</li>
               <li class="tag-mobile">${myWorksContent[index].tecnologies.tech3}</li>
-			  <li class="tag-mobile">${myWorksContent[index].tecnologies.tech4}</li>
+          <li class="tag-mobile">${myWorksContent[index].tecnologies.tech4}</li>
           </ul>
           <p class="work-discription-mobile">
-				${myWorksContent[index].description}
+        ${myWorksContent[index].description}
           </p>
           <div class="work-button-mobile">
                   <a href=${myWorksContent[index].linkVersion} class="work-detail-btn-mobile">
@@ -158,16 +157,15 @@ linkContactMe.addEventListener('click', () => {
   navMobile.classList.add('hidden');
   mobileMenuBtn.classList.remove('hidden');
 });
-console.log(popupBtns.length);
-for(let i=0; i < popupBtns.length; i++){
-	popupBtns[i].addEventListener('click', () => {
-		const myWork = document.createElement('div');
-		myWork.innerHTML = popupDiv(i);
-		document.body.appendChild(myWork);
-		console.log("here " + i);
-		const closePopupBtn = document.querySelector('.btn-close-pop-up');
-		closePopupBtn.addEventListener('click', () => {
-		document.body.removeChild(myWork);
-	});
-});
+
+for (let i = 0; i < popupBtns.length; i += 1) {
+  popupBtns[i].addEventListener('click', () => {
+    const myWork = document.createElement('div');
+    myWork.innerHTML = popupDiv(i);
+    document.body.appendChild(myWork);
+    const closePopupBtn = document.querySelector('.btn-close-pop-up');
+    closePopupBtn.addEventListener('click', () => {
+      document.body.removeChild(myWork);
+    });
+  });
 }
